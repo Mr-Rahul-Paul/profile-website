f5bf7a6a9f737f06ad42e7b8c91ac15b1b334f4b
Rahul Paul <179798584+Mr-Rahul-Paul@users.noreply.github.com>
2025-06-27 00:40:42 +0530

    fixed the backdrops

--- src/components/pages/main.tsx
+++ src/components/pages/main.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Achievements from "./achievements";
 import Education from "./education";
-import EmailandSkills from "./emailandskills";
+import EmailandSkills from "./Skills";
 import Telebot from "./telebot";
 import Hero from "./hero";
 
@@ -21,6 +21,13 @@ export default function Main() {
         width={1920}
         height={1080}
         className="absolute top-[100vh] left-0 w-full h-screen object-cover -z-10"
+      ></Image>
+      <Image
+        src="/BG3_BLACK.png"
+        alt="bg"
+        width={1920}
+        height={1080}
+        className="absolute top-[200vh] left-0 w-full h-[75vh] object-cover -z-10"
       ></Image>
       {/* Gradient overlay after BG2_BLACK.png */}
       <div className="relative max-w-[850px] mx-auto px-6 py-12 ">
