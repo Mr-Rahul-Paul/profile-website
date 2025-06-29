1fc2297648b5946d35d5136aab83910dd7437934
Rahul Paul <179798584+Mr-Rahul-Paul@users.noreply.github.com>
2025-06-29 19:23:57 +0530

    bg change

--- src/components/pages/main.tsx
+++ src/components/pages/main.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Achievements from "./achievements";
 import Education from "./education";
 import EmailandSkills from "./Skills";
@@ -7,8 +6,16 @@ import Hero from "./hero";
 
 export default function Main() {
   return (
-    <div className="min-h-screen bg-transparent text-foreground pt-21 flex flex-wrap">
-     
+    <div
+      className="min-h-screen bg-transparent text-foreground pt-21 flex flex-wrap"
+      style={{
+        backgroundImage: "url(/BG_BLACK.png)",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundAttachment: "fixed",
+      }}
+    >
       {/* Gradient overlay after BG2_BLACK.png */}
       <div className="relative max-w-[850px] mx-auto px-6 py-12 ">
         {/* Hero */}
